3c9c7907d66db8f840d009bf3b8819052a790b2c
Jerick-CM <jmangaluz@gmail.com>
2023-01-09 15:58:33 +0800

    initialize composer

---
+++ src/routes.php
@@ -0,0 +1,8 @@
+<?php
+// use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
+
+Route::get('calculator', function () {
+    echo "Hello from api calculator";
+});
